ea289ee40033ee52584b1c0e75b321365eccafa8
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-10-20 16:14:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+let infos = window.navigator;
+const info = document.getElementById("browser-info");
+
+info.innerText = `You are using ${infos.appName} ${infos.appVersion}`;
+
+document.body.appendChild(info);
